6075c17655e22574137fb601c64b64c5fb1088e1
ardasem <73624089+ardasem@users.noreply.github.com>
2023-12-23 16:19:12 +0300

    product page bestseller component fixed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 
 function ProductCard(props) {
-  const { imgUrl, category, name, oldPrice, price,options} = props;
+  const { imgUrl, category, name, oldPrice, price, options } = props;
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex-1">
-
-      <img src={imgUrl} alt="" />
+        <img className="w-full" src={imgUrl} alt="" />
       </div>
       <div className="flex flex-col justify-center items-center gap-[10px] p-[25px]">
-        <p className="font-['Montserrat'] text-[16px] font-[700] text-dark-blue">{category}</p>
-        <p className="font-['Montserrat'] text-[14px] font-bold text-md-grey">{name}</p>
+        <p className="font-['Montserrat'] text-[16px] font-[700] text-dark-blue">
+          {category}
+        </p>
+        <p className="font-['Montserrat'] text-[14px] font-bold text-md-grey">
+          {name}
+        </p>
         <section className="flex flex-row gap-[5px]">
-          <p className="font-['Montserrat'] text-[16px] font-bold text-light-grey">${oldPrice}</p>
-          <p className="font-['Montserrat'] text-[16px] font-bold text-md-green">${price}</p>
+          <p className="font-['Montserrat'] text-[16px] font-bold text-light-grey">
+            ${oldPrice}
+          </p>
+          <p className="font-['Montserrat'] text-[16px] font-bold text-md-green">
+            ${price}
+          </p>
         </section>
         {options && <img src="/assets/product-colors.png" alt="" />}
       </div>
--- src/components/productpage/BestSeller.jsx
+++ src/components/productpage/BestSeller.jsx
@@ -3,8 +3,8 @@ import ProductCard from "../ProductCard";
 
 function BestSeller() {
   return (
-    <div className="flex flex-col justify-center">
-      <div className="my-[48px] border-b-[1px] border-light-grey mx-[195px] ">
+    <div className="flex flex-col justify-center mx-[195px] my-[48px]">
+      <div className=" border-b-[1px] border-light-grey mb-[24px] ">
         <div className="py-[24px]">
           <p className="font-[Montserrat] text-[24px] font-[700] text-dark-blue">
             BESTSELLER PRODUCTS
@@ -12,69 +12,68 @@ function BestSeller() {
         </div>
       </div>
 
-      <div className="flex flex-col m-auto shrink-1">
-      <div className="flex flex-row  gap-x-[30px]">
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
+      <div className="flex flex-col">
+        <div className="flex flex-row  gap-x-[30px]">
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+        </div>
+        <div className="flex flex-row flex-1 gap-x-[30px]">
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+          <ProductCard
+            imgUrl={"/assets/product-cover-best.png"}
+            category={"English"}
+            name={"Graphic Design"}
+            oldPrice={16.88}
+            price={6.48}
+          />
+        </div>
       </div>
-      <div className="flex flex-row flex-1 gap-x-[30px]">
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-        <ProductCard
-          imgUrl={"/assets/product-cover-best.png"}
-          category={"English"}
-          name={"Graphic Design"}
-          oldPrice={16.88}
-          price={6.48}
-        />
-      </div>
-      </div>
-
     </div>
   );
 }
